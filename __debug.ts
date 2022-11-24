697f1f4f002425ec2d78bb8f6c0ce135b631df71
lamgr <lamgradon@Hotmail.com>
2022-11-24 16:14:33 +0800

    [feat] ui(frontend) and core service(backend) now can be booted from the same nodejs entry; updated log function

--- __debug.ts
+++ __debug.ts
@@ -2,17 +2,19 @@ import {NavigationService} from "./services/NavigationService";
 import {Loader} from "./core/UniversalPropertyLoader/Loader";
 import {YAMLWorker} from "./core/UniversalPropertyLoader/wokers/impl/YAMLWorker";
 import express from "express";
-import {Navigation} from "./services/instance/Navigation";
 import {NetworkResult} from "./core/NetworkResult";
 import {DBWorker} from "./core/UniversalPropertyLoader/wokers/impl/DBWorker";
-import {AuthService} from "./services/AuthService";
 import {ResultComplex} from "./core/Complex/impl/ResultComplex";
 import {InterfaceSchema} from "./InterfaceSchema";
 import {ErrorComplex} from "./core/Complex/impl/ErrorComplex";
-import {AuthedUser} from "./services/instance/AuthedUser";
 import jwt from "jsonwebtoken";
 
 import jwtsecret from "./stores/jwtsecret.json";
+import {AuthController} from "./controller/AuthController";
+import {NavigationController} from "./controller/NavigationController";
+import path from "path";
+
+import history from "connect-history-api-fallback";
 
 Loader.assignWorker(new YAMLWorker("server"), "static", "server");
 
@@ -21,7 +23,15 @@ const PORT = Loader.get<[number]>(["Port"], null, "static", "server")[0];
 const HOST = Loader.get<[string]>(["Hostname"], null, "static", "server")[0];
 SERVER.use(express.json());
 
-let unAuthedURLWhitelist = new Set<string>(["/auth", "/create", "/authMethod"]);
+// const UI = express();
+SERVER.use(express.static(path.resolve(__dirname, "../../frontend/ui/dist")));
+SERVER.use(history({
+    index: "/index.html"
+}));
+
+const DATE_FORMAT = new Intl.DateTimeFormat("zh-cn", {year: "numeric", month: "numeric", day: "numeric", hour: "2-digit", minute: "2-digit", second: "2-digit"})
+
+let unAuthedURLWhitelist = new Set<string>(["/", "/auth/auth", "/auth/create", "/auth/authMethod"]);
 let secret = jwtsecret["secret"];
 SERVER.use((req, res, next) => {
     res.setHeader("Cache-Control", "must-revalidate");
@@ -30,30 +40,41 @@ SERVER.use((req, res, next) => {
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
     );
-    console.log(`${req.ip} ${req.method} ${req.url} ${JSON.stringify([req.body, req.query, req.params])}`);
-    let indexOfDeli = req.url.indexOf("?");
-    let url = req.url.slice(0, indexOfDeli == -1 ? req.url.length + 1 : indexOfDeli);
-    if (!unAuthedURLWhitelist.has(url)) {
-        try {
-            res.locals.authInfo = jwt.verify(req.headers.authorization, secret);
+    if (req.url.startsWith("/api")) { // counter url rewrite
+        req.url = req.url.slice(4);
+        console.log(`[API_CALL] [${DATE_FORMAT.format(Date.now())}] ${req.ip} ${req.method} ${req.url} ${JSON.stringify([req.body, req.query, req.params])}`);
+        let indexOfDeli = req.url.indexOf("?");
+        let url = req.url.slice(0, indexOfDeli == -1 ? req.url.length + 1 : indexOfDeli);
+        if (!unAuthedURLWhitelist.has(url)) {
+            try {
+                res.locals.authInfo = jwt.verify(req.headers.authorization, secret);
+                next();
+            } catch (e) {
+                NetworkResult.send(
+                    new ErrorComplex("MISC", "NOT_AUTHED", "not authed, please auth first"),
+                    res
+                );
+                return;
+            }
+        }
+        let missingParams = InterfaceSchema.check(url, req);
+        if (!missingParams.length) {
             next();
-        } catch (e) {
+        } else {
             NetworkResult.send(
-                new ErrorComplex("MISC", "NOT_AUTHED", "not authed, please auth first"),
+                new ErrorComplex("MISC", "ARG_MISSING", `required fields ${[...missingParams]} is missing`),
                 res
             );
-            return;
         }
-    }
-    let missingParams = InterfaceSchema.check(url, req);
-    if (!missingParams.length) {
         next();
-    } else {
-        NetworkResult.send(
-            new ErrorComplex("MISC", "ARG_MISSING", `required fields ${[...missingParams]} is missing`),
-            res
-        );
+        return;
     }
+    console.log(`[OTHER] [${DATE_FORMAT.format(Date.now())}] ${req.ip} ${req.method} ${req.url} ${JSON.stringify([req.body, req.query, req.params])}`);
+    next();
+});
+
+SERVER.all("*", (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, "../../frontend/ui/dist/index.html"));
 });
 
 Loader.assignWorker(new YAMLWorker("fsnav"), "static", "fsnav");
@@ -63,60 +84,8 @@ Loader.assignWorker(new DBWorker("test", "user"), "db", "user");
 // todo: transfer userBeacon, userInstance via JWT
 ////
 
-SERVER.post("/create", (req, res, next) => {
-    // NetworkResult.send(AuthService.createAuth(req.body[""]))
-});
-
-SERVER.get("/authMethod", (req, res, next) => {
-    // use query here
-    NetworkResult.send(AuthService.getAuthMethod(req.params["userBeacon"]), res);
-});
-
-SERVER.post("/auth", (req, res, next) => {
-    NetworkResult.send(AuthService.auth(req.body["userBeacon"], req.body["userToken"]), res);
-});
-
-SERVER.post("/deauth", async (req, res, next) => {
-    NetworkResult.send(AuthService.deAuth(res.locals.authInfo["userBeacon"], res.locals.authInfo["userInstance"]), res);
-});
-
-SERVER.post("/deauthAll", (req, res, next) => {
-    NetworkResult.send(AuthService.deAuthAll(res.locals.authInfo["userBeacon"], res.locals.authInfo["userInstance"]), res);
-});
-
-SERVER.post("/assign", (req, res, next) => {
-    NetworkResult.send(
-        NavigationService.assignNavigation(res.locals.authInfo["userBeacon"], res.locals.authInfo["userInstance"]), res
-    );
-});
-
-SERVER.post("/remove", (req, res, next) => {
-    NetworkResult.send(
-        NavigationService.removeNavigation(res.locals.authInfo["userBeacon"], res.locals.authInfo["userInstance"],
-            req.body["instance"]), res
-    );
-});
-
-SERVER.post("/go", (req, res, next) => {
-    NetworkResult.send(
-        NavigationService.forward(res.locals.authInfo["userBeacon"], res.locals.authInfo["userInstance"],
-            req.body["instance"], req.body["where"]), res
-    );
-});
-
-SERVER.post("/back", (req, res, next) => {
-    NetworkResult.send(
-        NavigationService.backward(res.locals.authInfo["userBeacon"], res.locals.authInfo["userInstance"],
-            req.body["instance"]), res
-    );
-});
-
-SERVER.post("/tp", (req, res, next) => {
-    NetworkResult.send(
-        NavigationService.locate(res.locals.authInfo["userBeacon"], res.locals.authInfo["userInstance"],
-            req.body["instance"], req.body["fullPath"]), res
-    );
-});
+SERVER.use("/auth", AuthController.controller);
+SERVER.use("/nav", NavigationController.controller);
 
 SERVER.post("/debug/brwinst", (req, res, next) => {
    NetworkResult.send(new ResultComplex<{}>("DEBUG", Loader.operate<{}, {}>((instance) => {
@@ -124,6 +93,6 @@ SERVER.post("/debug/brwinst", (req, res, next) => {
    }, NavigationService.keys.NavigationInstanceTable)), res);
 });
 
-SERVER.listen(PORT, HOST, () => {
-    console.log(`server running on ${PORT}:${HOST}`);
+SERVER.listen(PORT, () => {
+    console.log(`core service running on ${PORT}`);
 });
